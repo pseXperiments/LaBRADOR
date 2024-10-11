9ccb6c70c1fdcdea2b6383da75b7683dfa093946
vimwitch <jchancehud@gmail.com>
2024-10-11 01:34:40 -0700

    feat: add conjugation automorphism

--- src/constraints.rs
+++ src/constraints.rs
@@ -33,6 +33,62 @@ mod test {
     }
 
     #[test]
+    fn conj_automorphism() {
+        // LaBRADOR page 6, conjugation automorphism
+        //
+        // polynomial implementation does not allow negative exponents
+        // we'll manually find a congruent value for x^-1
+        // for now we'll assert the ring modulus is x^n + 1
+        // with n >= 2
+        let m = DilithiumPolynomialRingElement::modulus();
+        let degree = m.degree();
+        assert!(degree >= 2);
+        for (i, coef) in m.coefficients.iter().enumerate() {
+            match i {
+                0 => assert_eq!(coef, &DilithiumRingElement::one()),
+                _ if i == degree => assert_eq!(coef, &DilithiumRingElement::one()),
+                _ => assert_eq!(coef, &DilithiumRingElement::zero()),
+            }
+        }
+        // we'll determine a polynomial with positive exponents
+        // that is congruent to x^-1
+        // in Z_q[x]/(x^n + 1)
+        // -x^(n-1) = x^-1
+        // x^-z = -x^(n-z)
+        let a = DilithiumPolynomialRingElement::sample_rand(&mut rand::thread_rng());
+        let mut out_coefs = vec![DilithiumRingElement::zero(); degree];
+        for (i, coef) in a.polynomial().coefficients.iter().enumerate() {
+            if i == 0 {
+                // constant term is unchanged
+                // x^-1^0 = 1
+                out_coefs[0] = coef.clone();
+                continue;
+            }
+            let j = degree - i;
+            out_coefs[j] = -coef.clone();
+        }
+        let a_automorphism = DilithiumPolynomialRingElement(Polynomial {
+            coefficients: out_coefs,
+        });
+
+        let b = DilithiumPolynomialRingElement::sample_rand(&mut rand::thread_rng());
+
+        let ct = (a_automorphism * b.clone()).polynomial().coefficients[0];
+
+        // coefficient dot product of a and b coefficients
+        let coef_dot_prod = {
+            let mut out = DilithiumRingElement::zero();
+            let zero = DilithiumRingElement::zero();
+            for i in 0..(usize::max(a.polynomial().degree(), b.polynomial().degree()) + 1) {
+                out += a.polynomial().coefficients.get(i).unwrap_or(&zero).clone()
+                    * b.polynomial().coefficients.get(i).unwrap_or(&zero).clone();
+            }
+            out
+        };
+        assert_eq!(coef_dot_prod, ct);
+    }
+
+    #[test]
     fn generate_constraints() {
         // in the paper n = row count and r = column count ?
         let n = 8;
